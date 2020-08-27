708848f98821cee8282311190efe72c8e3d1b362
Gislaine Costa Pereira <62241763+gislainecosta@users.noreply.github.com>
2020-08-27 18:32:35 -0300

    Componetização do Aplicativo (Header, Main e Footer)

--- marvel-jogo-da-velha/src/app/app.module.ts
+++ marvel-jogo-da-velha/src/app/app.module.ts
@@ -2,10 +2,22 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './components/app/app.component';
+import { HeaderComponent } from './components/header/header.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { CardComponent } from './components/header/card/card.component';
+import { MainComponent } from './components/main/main.component';
+import { GameComponent } from './components/main/game/game.component';
+import { GamerComponent } from './components/main/gamer/gamer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    CardComponent,
+    MainComponent,
+    GameComponent,
+    GamerComponent
   ],
   imports: [
     BrowserModule
